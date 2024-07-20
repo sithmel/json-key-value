95666472fbbe7410f2da38cb0fea5ed963aed5a9
Maurizio Lupo <maurizio.lupo@gmail.com>
2024-07-20 13:00:03 +0100

    ObjectToSequence maxdepth

--- src/ObjectToSequence.mjs
+++ src/ObjectToSequence.mjs
@@ -2,13 +2,22 @@
 import { isArrayOrObject } from "./utils.mjs"
 export default class ObjectToSequence {
   /**
+   * Convert a stream of characters (in chunks) to a sequence of path/value pairs
+   * @param {{ maxDepth?: number }} options
+   */
+  constructor(options = {}) {
+    const { maxDepth = Infinity } = options
+    this.maxDepth = maxDepth
+  }
+
+  /**
    * parse a json or json fragment
    * @param {any} obj
    * @param {import("../types/baseTypes").JSONPathType} currentPath
    * @returns {Iterable<[import("../types/baseTypes").JSONPathType, import("../types/baseTypes").JSONValueType]>}
    */
   *iter(obj, currentPath = []) {
-    if (isArrayOrObject(obj)) {
+    if (isArrayOrObject(obj) && currentPath.length < this.maxDepth) {
       let pathSegmentsAndValues
       if (Array.isArray(obj)) {
         yield [currentPath, []]
--- test/ObjectToSequence.test.mjs
+++ test/ObjectToSequence.test.mjs
@@ -5,7 +5,7 @@ import pkg from "zunit"
 import ObjectToSequence from "../src/ObjectToSequence.mjs"
 import SequenceToObject from "../src/SequenceToObject.mjs"
 
-const { describe, it, beforeEach } = pkg
+const { describe, it, oit, beforeEach } = pkg
 
 describe("ObjParser", () => {
   let parse
@@ -28,5 +28,59 @@ describe("ObjParser", () => {
     const obj = { a: 1, b: 2, array: [0, 1, 2] }
     parse(obj)
     assert.deepEqual(builder.object, obj)
+  })
+  describe("nesting with maxDepth", () => {
+    let parserIter
+    beforeEach(() => {
+      const parser = new ObjectToSequence({ maxDepth: 1 })
+      parserIter = (obj) => Array.from(parser.iter(obj))
+    })
+
+    it("works with object nested into object (1)", () => {
+      const seq = parserIter({ test1: { test2: 1 } })
+      assert.deepEqual(seq, [
+        [[], {}],
+        [["test1"], { test2: 1 }],
+      ])
+    })
+
+    it("works with object nested into object (2)", () => {
+      const seq = parserIter({ test1: { test2: 1 }, test3: 2 })
+      assert.deepEqual(seq, [
+        [[], {}],
+        [["test1"], { test2: 1 }],
+        [["test3"], 2],
+      ])
+    })
+
+    it("works with object nested into arrays (1)", () => {
+      const seq = parserIter([{ test1: 1 }, { test2: 2 }])
+      assert.deepEqual(seq, [
+        [[], []],
+        [[0], { test1: 1 }],
+        [[1], { test2: 2 }],
+      ])
+    })
+
+    it("works with object nested into arrays (2)", () => {
+      const seq = parserIter([{ test1: [1, "xyz"] }, { test2: 2 }])
+      assert.deepEqual(seq, [
+        [[], []],
+        [[0], { test1: [1, "xyz"] }],
+        [[1], { test2: 2 }],
+      ])
+    })
+
+    it("works with object nested into arrays (3)", () => {
+      const seq = parserIter([
+        [1, 2, 3],
+        [4, 5, 6],
+      ])
+      assert.deepEqual(seq, [
+        [[], []],
+        [[0], [1, 2, 3]],
+        [[1], [4, 5, 6]],
+      ])
+    })
   })
 })
